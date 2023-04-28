d076237bdea1bd23f0963480de5544d183bc4fd8
rohitjethoe <rohitjethoe@proton.me>
2023-04-28 22:47:12 +0200

    vite config target esnext.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  esbuild: {
+    target: 'esnext'
+  },
   plugins: [vue()],
   resolve: {
     alias: {
