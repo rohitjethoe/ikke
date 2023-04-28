a3d2e63348fac42c4e5d0ac4450d12136013d48b
rohitjethoe <rohitjethoe@proton.me>
2023-04-28 23:17:32 +0200

    styling.

--- src/App.vue
+++ src/App.vue
@@ -17,12 +17,12 @@
 
       <div>
         <div class="pt-36">
-          <h2 class="uppercase font-bold text-gassyGray text-sm">
+          <h2 class="uppercase font-bold text-gassyGray text-xs">
             Uitgelichte projecten
           </h2>
 
           <div class="pt-12 pb-24">
-            <p class="font-cormorant w-5/6 mx-auto text-sm sm:text-base">Reddit Karma Farm is een desktop applicatie die geautomatiseerd karmapunten op Reddit verzamelt. De punten worden verzamelt door comments te plaatsen. Volledig gebouwt op JavaScript met de welbekende libraries Puppeteer en Electron met Vue.js.</p>
+            <p class="font-cormorant w-5/6 mx-auto text-base sm:text-lg">Reddit Karma Farm is een desktop applicatie die geautomatiseerd karmapunten op Reddit verzamelt. De punten worden verzamelt door comments te plaatsen. Volledig gebouwt op JavaScript met de welbekende libraries Puppeteer en Electron met Vue.js.</p>
             <div class="flex mt-8 w-[200vw]">
               <div class="w-[350px]">
                 <img src="./assets/reddit-karma-farm.gif" alt="">
@@ -43,7 +43,7 @@
           </div>
 
           <div class="pt-12 pb-24">
-            <p class="font-cormorant w-5/6 mx-auto text-right text-sm sm:text-base">Bol.com Affiliate Web API is een backendsysteem gebouwd in Node.js die gebruikt maakt caching om de JWT tokens op te slaan. Zo worden er aan de regels voldaan van Bol.com om zo min mogelijk API calls te maken.</p>
+            <p class="font-cormorant w-5/6 mx-auto text-right text-base sm:text-lg">Bol.com Affiliate Web API is een backendsysteem gebouwd in Node.js die gebruikt maakt caching om de JWT tokens op te slaan. Zo worden er aan de regels voldaan van Bol.com om zo min mogelijk API calls te maken.</p>
             <div class="flex mt-8 w-[200vw]">
               <div class="w-[350px]">
                 <p class="my-4 text-sm"><span class="text-gassyGray text-xs">een</span>&nbsp; &nbsp; &nbsp; &nbsp;nodejs bol api</p>
@@ -67,7 +67,7 @@
 
       <div>
         <div class="pt-36">
-          <h2 class="uppercase font-bold text-gassyGray text-sm">
+          <h2 class="uppercase font-bold text-gassyGray text-xs">
             Werkervaringen
           </h2>
           <div class="pt-12">
